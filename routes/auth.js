4ab119cbfbdfd9db7cc083371fd795797aa81b90
minhthu1902 <minhthu1902@gmail.com>
2025-05-23 14:52:26 -0500

    change on some minor errors

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
 const clothingItem = require("./clothingItems");
 const userRouter = require("./users");
-const { UNAUTHORIZED } = require("../utils/errors");
+const { NOT_FOUND } = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItem);
 
-router.use((req, res) => {
-  res.status(UNAUTHORIZED).send({ message: "Router not found" });
+router.use((NOT_FOUND).send({ message: "Router not found" });
 });
 
 // const { login, createUser } = require("../controllers/users");
